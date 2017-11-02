192d590c82b3fd235a2cac9477e89240b49be9f4
fand <fand.gmork@gmail.com>
2017-11-02 10:55:24 +0900

    Reverse the order of props and injectedProps to prioritize props

--- index.tsx
+++ index.tsx
@@ -34,7 +34,7 @@ export function react2angular<Props>(
       }
       render() {
         // TODO: rm any when https://github.com/Microsoft/TypeScript/pull/13288 is merged
-        render(<Class {...(this.props as any)} {...this.injectedProps} />, this.$element[0])
+        render(<Class {...this.injectedProps} {...(this.props as any)} />, this.$element[0])
       }
       componentWillUnmount() {
         unmountComponentAtNode(this.$element[0])
