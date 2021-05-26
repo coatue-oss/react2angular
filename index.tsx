a40076e67a912ee262f801b11513d85fde37f762
Andrea Giannoni <a.giannoni@domotz.com>
2021-05-26 12:03:48 +0200

    updating props on $onInit

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import { IAugmentedJQuery, IComponentOptions } from 'angular'
 import fromPairs = require('lodash.frompairs')
 import NgComponent from 'ngcomponent'
+import * as PropTypes from 'prop-types'
 import * as React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
-
 /**
  * Wraps a React component in Angular. Returns a new Angular component.
  *
@@ -24,8 +24,12 @@ export function react2angular<Props>(
     || (Class.propTypes && Object.keys(Class.propTypes) as (keyof Props)[])
     || []
 
+  const getPropType = (name: keyof Props): any => {
+    return !!Class.propTypes ? Class.propTypes[name] : null
+  }
   return {
-    bindings: fromPairs(names.map(_ => [_, '<'])),
+    bindings: fromPairs(names.map(name => [name, getPropType(name) === PropTypes.string ? '@' : '<'])),
+    // bindings: fromPairs(names.map(_ => [_, '<'])),
     controller: ['$element', ...injectNames, class extends NgComponent<Props> {
       static get $$ngIsClass() {
         return true
@@ -37,6 +41,11 @@ export function react2angular<Props>(
         this.injectedProps = {}
         injectNames.forEach((name, i) => {
           this.injectedProps[name] = injectedProps[i]
+        })
+      }
+      $onInit() {
+        names.forEach((name) => {
+          this.props[name] = (this as any)[name]
         })
       }
       render() {
